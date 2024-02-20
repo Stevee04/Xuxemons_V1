422379691a09eb7182b92e3204a67aedf3375e00
Stevee04 <104826601+Stevee04@users.noreply.github.com>
2024-02-20 19:19:03 +0100

    modified css

--- angular_routes/src/app/app.component.spec.ts
+++ angular_routes/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'PracticaEjemplo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PracticaEjemplo');
+    expect(app.nombre).toEqual('PracticaEjemplo');
   });
 
   it('should render title', () => {
